a78a4e8b8ed22731b5858db62e1a9f2a05a75c31
ForestWithLin <841056253@qq.com>
2019-10-09 16:38:08 +0800

    test file

---
+++ test.py
@@ -0,0 +1,13 @@
+class demo:
+    def __init__(self):
+        print("123")
+
+    def __test(self):
+        print("345")
+
+    def main(self):
+        self.__test()    
+
+t = demo()
+t.main()
+
